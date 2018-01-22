1ba9675c537c30c2e82a0bb7ffabb2d80bde5785
xcv58 <i@xcv58.com>
2018-01-21 21:01:44 -0800

    Add --format, --number and libs.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,38 @@
 #!/usr/bin/env node
 import 'babel-core/register'
 import 'babel-polyfill'
-import Binance from 'binance-api-node'
 import yarngs from 'yargs'
+import { getCandles } from './libs'
 
-const client = Binance()
-
-const { argv } = yarngs
-
-client.candles({ symbol: 'ETHBTC' })
-.then((candles) => {
-  console.log(candles)
+export default yarngs
+.command(
+  '$0 [symbols..]',
+  'Collect candles for symbol(s)',
+  {},
+  (argv) => {
+    const { symbols, interval, number } = argv
+    symbols.forEach((symbol) => {
+      getCandles({ symbol, interval, number })
+    })
+  })
+.option('format', {
+  alias: 'f',
+  describe: 'the output format',
+  default: 'csv',
+  choices: [
+    'csv',
+    'json'
+  ]
 })
-console.log(argv)
-console.log('Hello World!')
+.option('number', {
+  alias: 'n',
+  describe: 'the number of candles for each symbol, the result will round up by 500',
+  default: '1500',
+  type: 'number'
+})
+.option('interval', {
+  alias: 'i',
+  describe: 'the interval of candles',
+  default: '5m',
+  choices: [ '1m', '3m', '5m', '15m', '30m', '1h', '2h', '4h', '6h', '8h', '12h', '1d', '3d', '1w', '1M' ]
+}).argv
---
+++ src/libs.js
@@ -0,0 +1,6 @@
+// import Binance from 'binance-api-node'
+// const client = Binance()
+
+export const getCandles = ({ symbol, interval, number }) => {
+  console.log(`Get ${number} candles for ${symbol} with interval: ${interval}`)
+}
